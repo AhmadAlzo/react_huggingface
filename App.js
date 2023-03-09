71e0e2f4f671a3ed54a304a06fcd12e458b6c3e4
AhmadAlzo <124163736+AhmadAlzo@users.noreply.github.com>
2023-03-09 05:21:51 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -50,6 +50,39 @@ async function queryconversation(data) {
 // 	return result;
 // }
 
+async function queryimagine(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4",
+		{
+			headers: { Authorization: "Bearer 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" },
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.blob();
+	return result;
+}
+////////////////////////////
+
+////function
+
+///////////////////////////
+
+function Result(props){
+  if(props.element.type === "image"){
+    const objectURL = URL.createObjectURL(props.element.data)
+    const alt ="image"+props.num
+    return (
+      <>
+        <img src={objectURL} alt={alt}/>
+      </>
+    )
+  }else{
+    return props.element.data
+  }
+}
+
+
 
 //////////////////////////
 
@@ -66,33 +99,58 @@ function App() {
   // const [ answer,setanswer] = useState([])
   
   function click(){
-    queryconversation({"inputs": {
-      "past_user_inputs": questions,
-      "generated_responses": answer,
-      "text": input
-    }}).then((response) => {
-      if (response.generated_text!==undefined){
-        // querygeneration({"inputs": response.generated_text}).then((response2) => {
-        //   if (response2 !== undefined){
-        //     const tab = [...data,[input,response2[0].generated_text]]
-        //     setdata(tab)
-        //   }
-        // })
-        const tab = [...data,[input,response.generated_text]]
+    if (input.startsWith("#imagine#:")){
+      queryimagine({"inputs": input.slice(10,input.length)}).then((response) => {
+        const tab = [...data,[input.slice(10,input.length),{data:response,type:"image"}]]
         setdata(tab)
-        // let interque = [...questions,input]
-        // setquestionst(interque)
-        // let interansw = [...answer,response.generated_text]
-        // setanswer(interansw)
-        questions.push(input)
-        answer.push(response.generated_text)
-        setinput("")
-      }
-    })
+        setinput("#imagine#:")
+      });
+    }else{
+      queryconversation({"inputs": {
+        "past_user_inputs": questions,
+        "generated_responses": answer,
+        "text": input
+      }}).then((response) => {
+        if (response.generated_text!==undefined){
+          // querygeneration({"inputs": response.generated_text}).then((response2) => {
+          //   if (response2 !== undefined){
+          //     const tab = [...data,[input,response2[0].generated_text]]
+          //     setdata(tab)
+          //   }
+          // })
+          const tab = [...data,[input,{data:response.generated_text,type:"text"}]]
+          setdata(tab)
+          // let interque = [...questions,input]
+          // setquestionst(interque)
+          // let interansw = [...answer,response.generated_text]
+          // setanswer(interansw)
+          questions.push(input)
+          answer.push(response.generated_text)
+          setinput("")
+        }
+      })
+    }
+    
   }
   function change(e){
     setinput(e.target.value)
   }
+  // function makeImage(element){
+  //   if(element.type === "image"){
+  //     const now = new Date();
+  //     const time = now.toLocaleTimeString();
+      
+  //     const objectURL = URL.createObjectURL(element.data)
+  //     const alt ="image"+time
+  //     return (
+  //       <>
+  //         <img src={objectURL} alt={alt}/>
+  //       </>
+  //     )
+  //   }else{
+  //     return element.data
+  //   }
+  // }
   return (
     <div className="App">
       <div className="cont"> 
@@ -101,7 +159,8 @@ function App() {
             return(
               <div className="container" key={index}>
                 <div className="question">{e[0]}</div>
-                <div className="answer">{e[1]}</div>
+                {/* <div className="answer">{makeImage(e[1])}</div> */}
+                <div className="answer"><Result element={e[1]} num={index}/></div>
               </div>
             )
           })}
@@ -114,5 +173,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
